18f4dc1511a8b92e50d4371fc21427bb4fe92cb9
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 13:58:51 +0300

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,10 +1,47 @@
-wc -l ledian8 1>ledian8 2>/dev/null
+cd ~/lab0
+chmod 777 ledian8
+chmod 777 cyndaquil6
+wc -l ledian8 1> ledian8 2> /dev/null
+chmod 404 ledian8
+chmod 755 cyndaquil6
+
 #wc -l **/*o
-ls -lR 2>/dev/null| sort -k 2 -n | grep ".......... " 2>/dev/null | head
+
+ls -lR 2> /dev/null| sort -k 2 -n | grep ".......... " 2>/dev/null | head
+
+chmod 777 bellsprout3
+chmod 777 bellsprout3/vanillish
 cat -n ~/lab0/bellsprout3/vanillish | sort -r
+chmod ugo-wx vanillish
+chmod 512 bellsprout3
+
+
+chmod 777 cyndaquil6
+chmod 777 cyndaquil6/litwick
 cat -n ~/lab0/cyndaquil6/litwick | sort -r 
+chmod 062 cyndaquil6/litwick
+chmod 755 cyndaquil6
+
+
+chmod 777 cyndaquil6
+chmod 777 cyndaquil6/haxorus
 cat -n ~/lab0/cyndaquil6/haxorus | sort -r 
+chmod 400 cyndaquil6/haxorus
+chmod 755 cyndaquil6
+
+
+chmod 777 cyndaquil6
+chmod 777 cyndaquil6/mothim
 cat -n ~/lab0/cyndaquil6/mothim | sort -r 
+
+
+chmod 777 tympole3
+chmod 777 tympole3/sealeo
 cat -n ~/lab0/tympole3/sealeo | sort -r 
+chmod ugo+r-wx tympole3/sealeo
+chmod 311 tympole3
+
+
 ls -lR 2> /dev/null | grep "se" 2> /dev/null | sort -k 9 -r | tail -2
+
 ls 2> /tmp/errorlog | grep -v / 2> /tmp/errorlog | sort
