5d58d0e9bac6e2e324b80389f86b5ca903a199b2
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 13:23:55 +0300

    Update script3_3.sh

--- script3_3.sh
+++ script3_3.sh
@@ -29,39 +29,43 @@ chmod 512 bellsprout3
 chmod 311 tympole3
 
 
-chmod 777 servine2
-cp servine2 ~/lab0/tympole3/cascoon/
-chmod 664 servine2
+# chmod 777 servine2
+# chmod 777 tympole3
+# chmod 777 ~/lab0/tympole3/cascoon/
+# cp servine2 ~/lab0/tympole3/cascoon/
+# chmod 664 servine2
+# chmod 311 tympole3
+# chmod 571 cascoon
 
 
-chmod 777 bellsprout3
-ln -s ~/lab0/bellsprout3 Copy_83
-chmod 512 bellsprout3
+# chmod 777 bellsprout3
+# ln -s ~/lab0/bellsprout3 Copy_83
+# chmod 512 bellsprout3
 
 
-chmod 777 ledian8
-chmod 777 cyndaquil6
-ln -s ~/lab0/ledian8 ~/lab0/cyndaquil6/haxorusledian
-chmod 404 ledian8
-chmod 755 cyndaquil6
+# chmod 777 ledian8
+# chmod 777 cyndaquil6
+# ln -s ~/lab0/ledian8 ~/lab0/cyndaquil6/haxorusledian
+# chmod 404 ledian8
+# chmod 755 cyndaquil6
 
 
-chmod 777 ledian8
-chmod 777 bellsprout3
-ln ~/lab0/ledian8 ~/lab0/bellsprout3/vanillishledian
-chmod 404 ledian8
-chmod 512 bellsprout3
+# chmod 777 ledian8
+# chmod 777 bellsprout3
+# ln ~/lab0/ledian8 ~/lab0/bellsprout3/vanillishledian
+# chmod 404 ledian8
+# chmod 512 bellsprout3
 
 
-chmod 777 bellsprout3
-chmod 777 loudred 
-cd bellsprout3
-cat ~/lab0/bellsprout3/loudred ~/lab0/bellsprout3/loudred > ~/lab0/ledian8_91
-chmod u+rw,g+w-rx,o-rwx loudred
-cd ..
-chmod 512 bellsprout3
+# chmod 777 bellsprout3
+# chmod 777 loudred 
+# cd bellsprout3
+# cat ~/lab0/bellsprout3/loudred ~/lab0/bellsprout3/loudred > ~/lab0/ledian8_91
+# chmod u+rw,g+w-rx,o-rwx loudred
+# cd ..
+# chmod 512 bellsprout3
 
 
-chmod 777 servine2
-cat ~/lab0/servine2 > ~/lab0/minccinoservine
-chmod 664 servine2
+# chmod 777 servine2
+# cat ~/lab0/servine2 > ~/lab0/minccinoservine
+# chmod 664 servine2
