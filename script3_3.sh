51a3c861a922c6dc95ba1751833f5ed76bb97409
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 13:21:27 +0300

    Update script3_3.sh

--- script3_3.sh
+++ script3_3.sh
@@ -1,40 +1,50 @@
 cd lab0
 
+chmod 777 tympole3
 chmod 777 bellsprout3
 chmod 777 bellsprout3/charmander
 chmod 777 bellsprout3/accelgor
 chmod 777 bellsprout3/loudred
 chmod 777 bellsprout3/scyther
 chmod 777 bellsprout3/yanma
+
 mkdir -p bellsprout3/charmander/bellsprout3 &&                                                                                   
 ln -s charmander bellsprout3/charmander/bellsprout3/charmander &&
 cp -a . bellsprout3/charmander
+
 cd ~/lab0/bellsprout3/charmander/bellsprout3
+
 rm charmander
 mkdir charmander
 cd .. 
 cd .. 
-cd .. 
+
 chmod 006 accelgor
 chmod u+rw,g+w-rx,o-rwx loudred
 chmod 577 charmander
 chmod 736 scyther
 chmod 512 yanma
+cd .. 
 chmod 512 bellsprout3
+chmod 311 tympole3
+
 
 chmod 777 servine2
 cp servine2 ~/lab0/tympole3/cascoon/
 chmod 664 servine2
 
+
 chmod 777 bellsprout3
 ln -s ~/lab0/bellsprout3 Copy_83
 chmod 512 bellsprout3
+
 
 chmod 777 ledian8
 chmod 777 cyndaquil6
 ln -s ~/lab0/ledian8 ~/lab0/cyndaquil6/haxorusledian
 chmod 404 ledian8
 chmod 755 cyndaquil6
+
 
 chmod 777 ledian8
 chmod 777 bellsprout3
