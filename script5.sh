9f900719e35daf65e7cfb401f12a88e4f86bf105
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 14:11:09 +0300

    Create script5.sh

---
+++ script5.sh
@@ -0,0 +1,27 @@
+cd ~/lab0
+
+chmod 777 ledian8
+rm -f ledian8
+
+
+chmod 777 tympole3/minccino
+rm -f ~/lab0/tympole3/minccino
+chmod 311 tympole3
+
+
+chmod 777 cyndaquil6
+chmod 777 cyndaquil6/haxorusledian
+rm -f ~/lab0/cyndaquil6/haxorusledian
+chmod 755 cyndaquil6
+
+
+chmod 777 bellsprout3
+rm -f ~/lab0/bellsprout3/vanillishledian
+chmod 512 bellsprout3
+
+chmod -R 777 tympole3
+rm -rf ~/lab0/tympole3
+
+chmod 777 bellsprout3
+rm -rf ~/lab0/bellsprout3/scyther
+chmod 512 bellsprout3
