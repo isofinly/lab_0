f15a6da4dd42b8a7d27f3eb95de1f197c2eb58a7
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 10:54:58 +0300

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,28 @@
+cd ..
+chmod 512 bellsprout3
+cd bellsprout3
+chmod 006 accelgor
+chmod u+rw,g+w-rx,o-rwx loudred
+chmod 577 charmander
+chmod ugo-wx vanillish
+chmod 736 scyther
+chmod 512 yanma
+cd ..
+chmod 755 cyndaquil6
+cd cyndaquil6
+chmod 062 litwick
+chmod 400 haxorus
+chmod 524 gardevoir
+chmod 620 mothim
+cd .. 
+chmod u+rw,g+w-r,o-rwx galvantula8
+chmod 404 ledian8
+chmod 664 servine2
+chmod 311 tympole3
+cd tympole3
+chmod 571 cascoon 
+chmod ugo+r-wx sealeo 
+chmod 577 hitmonlee
+chmod 060 minccino
+chmod 751 conkeldurr
+cd ..
