70a61c4002c2df582d12feca294188b5457b5293
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 10:56:35 +0300

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,7 @@
+cp -R * bellsprout3 /tmp/lima/lab0/bellsprout3/charmander/
+cp servine2 /tmp/lima/lab0/tympole3/cascoon/
+ln -s /tmp/lima/lab0/bellsprout3 Copy_83
+ln -s /tmp/lima/lab0/ledian8 /tmp/lima/lab0/cyndaquil6/haxorusledian
+ln /tmp/lima/lab0/ledian8 /tmp/lima/lab0/bellsprout3/vanillishledian/
+cat /tmp/lima/lab0/bellsprout3/loudred /tmp/lima/lab0/bellsprout3/loudred > /tmp/lima/lab0/ledian8_91
+cat /tmp/lima/lab0/servine2 > /tmp/lima/lab0/minccinoservine
