728b50767e8608360e302729880e3e4549b57233
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 10:54:21 +0300

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,52 @@
+mkdir lab0
+cd lab0
+mkdir bellsprout3
+cd bellsprout3
+touch accelgor 
+echo "тип покемона BUG NONE" >>/tmp/lima/lab0/bellsprout3/accelgor
+touch loudred 
+echo "ходы Body Stam Counter
+Defense Curt Double-Edge Dynamicpunch Endeavor Fire Punch Hyper Voice
+Ice Punch Icy Wind Low kick Mega kick Mega Punch Mud-Stap Psych up
+Rollout Seismic Toss Shock Wave Steep таи Snore Thunderpunch Uproar
+Water Putse Zen Headbutt" >>/tmp/lima/lab0/bellsprout3/loudred
+mkdir charmander 
+touch vanillish
+echo "тип покемона ICE
+NONE" >>/tmp/lima/lab0/bellsprout3/vanillish
+mkdir scyther 
+mkdir yanma
+cd ..
+mkdir cyndaquil6 
+cd cyndaquil6
+touch litwick
+echo "способности Haunt Btaze Flash Fire Flame
+Body" >>/tmp/lima/lab0/cyndaquil6/litwick
+touch haxorus 
+echo "weigth=232.6 height=71.0 atk=15
+def=9" >>/tmp/lima/lab0/cyndaquil6/haxorus
+mkdir gardevoir 
+touch mothim
+echo "способности Protect Bug Bite Hidden Power Confusion
+Gust Poisonpowder Psybeam Camouflage Silver Wind Air Slash Psychic Bug
+Buzz Quiver Dance" >>/tmp/lima/lab0/cyndaquil6/mothim
+cd .. 
+touch galvantula8 
+echo "живет Cave Forest" >>/tmp/lima/lab0/galvantula8
+touch ledian8 
+echo "способности
+Swarm Mach Speed Earty Bird Frisk" >>/tmp/lima/lab0/ledian8
+touch servine2 
+echo "satk=6 sdef=8
+spd=8"  >>/tmp/lima/lab0/servine2
+mkdir tympole3 
+cd tympole3
+mkdir cascoon 
+touch sealeo 
+echo "возможности Overland=6 Surface=9 Undewater=7 Jump=3
+Power=3 Intelligence=4 Icestep=0" >>/tmp/lima/lab0/sealeo
+mkdir hitmonlee 
+touch minccino 
+echo "Развитые способности Skill
+link" >> /tmp/lima/lab0/minccino
+mkdir conkeldurr
