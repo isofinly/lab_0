d63a36e193529b6bb4329bb5d5469d488016c368
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-25 12:21:29 +0300

    Create script3_3.sh

---
+++ script3_3.sh
@@ -0,0 +1,35 @@
+cd lab0
+chmod  777 bellsprout3
+cp -R * bellsprout3 ~/lab0/bellsprout3/charmander/
+chmod 512 bellsprout3
+
+chmod 777 servine2
+cp servine2 ~/lab0/tympole3/cascoon/
+chmod 664 servine2
+
+chmod 777 bellsprout3
+ln -s ~/lab0/bellsprout3 Copy_83
+chmod 512 bellsprout3
+
+chmod 777 ledian8
+chmod 777 cyndaquil6
+chmod 777 haxorusledian
+ln -s ~/lab0/ledian8 ~/lab0/cyndaquil6/haxorusledian
+chmod 404 ledian8
+chmod 755 cyndaquil6
+
+chmod 777 ledian8
+chmod 777 bellsprout3
+ln ~/lab0/ledian8 ~/lab0/bellsprout3/vanillishledian
+chmod 404 ledian8
+chmod 512 bellsprout3
+
+chmod 777 bellsprout3
+chmod 777 loudred 
+cat ~/lab0/bellsprout3/loudred ~/lab0/bellsprout3/loudred > ~/lab0/ledian8_91
+chmod 512 bellsprout3
+chmod u+rw,g+w-rx,o-rwx loudred
+
+chmod 777 servine2
+cat ~/lab0/servine2 > ~/lab0/minccinoservine
+chmod 664 servine2
