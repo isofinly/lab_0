a89ffac2ac5b9cf50299f746ec90312f8fc456d9
Average senior assembler dungeon master <107208596+isofinly@users.noreply.github.com>
2022-09-29 00:01:39 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 mkdir -p bellsprout3/charmander/bellsprout3 &&                                                                                   
 ln -s charmander bellsprout3/charmander/bellsprout3/charmander &&
 cp -a . bellsprout3/charmander && cd ~/lab0/bellsprout3/charmander/bellsprout3 && rm charmander && mkdir charmander && cp ~/lab0/bellsprout3/accelgor ~/lab0/bellsprout3/charmander/bellsprout3/ 2> /dev/null
+
+
